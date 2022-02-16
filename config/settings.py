c19b30c11a227d0f7c5288088338a7b2deb38c76
Rushil Umaretiya <rushilwiz@gmail.com>
2022-02-16 16:21:21 -0500

    fix: fixed for prod

--- config/settings.py
+++ config/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://relish.monster']
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
@@ -130,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
